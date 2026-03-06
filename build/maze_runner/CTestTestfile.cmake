2c4ebe0f0411f676fb76214051b5e0730ec710d2
Liubai-byte <hanhhh0101@outlook.com>
2026-03-06 19:41:51 +0800

    Delete build directory

--- build/maze_runner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eai/maze_ws/src/maze_runner
-# Build directory: /home/eai/maze_ws/build/maze_runner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
